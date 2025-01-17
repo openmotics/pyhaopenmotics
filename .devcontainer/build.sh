2b441d0b2eb5510659abf8be0bc84fd9620b5342
Wouter Coppens <wouter.coppens@gmail.com>
2025-01-17 23:21:09 +0100

    Fixes + devcontainer

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # This is a script used by the devcontainer to build the project
 
-. ${NVM_DIR}/nvm.sh 
-nvm install 
-nvm use 
+. ${NVM_DIR}/nvm.sh
+nvm install
+nvm use
 
-npm install 
+npm install
 
 sudo rm /usr/lib/python3.12/EXTERNALLY-MANAGED
 
-/usr/local/py-utils/bin/poetry install 
+/usr/local/py-utils/bin/poetry install
 /usr/local/py-utils/bin/poetry run pre-commit install
 
 /usr/bin/pip install python-dotenv
